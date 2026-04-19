64f2eebdd53676c8edc775b9edfa503f27c2a913
rihem <rihem.matoussi@esprit.tn>
2026-04-19 15:56:56 +0100

    Azure prod env config

--- product-front/src/app/services/product.service.ts
+++ product-front/src/app/services/product.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../models/product';
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  private baseUrl = 'http://localhost:8081/api/products';
+  private baseUrl = `${environment.apiUrl}/products`;
 
   constructor(private http: HttpClient) {}
 
