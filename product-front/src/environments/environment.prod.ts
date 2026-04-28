8b9dcd7b3a7a3062127477ab5683bc2f33bc5a4c
Rihem Matoussi <61407411+rehamtaieb@users.noreply.github.com>
2026-04-28 16:01:58 +0100

    Update environment.prod.ts

--- product-front/src/environments/environment.prod.ts
+++ product-front/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://myapp-backend-bgcpgcgrdtfzezc6.austriaeast-01.azurewebsites.net/api'
+  apiUrl: 'https://app-backend-fbc4d6ghfwfwbwhv.austriaeast-01.azurewebsites.net/api'
 };
